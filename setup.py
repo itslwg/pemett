427a025c381d36b40e968597ff3ae2a00e8e7ffe
Ludde <ludviggerdin@gmail.com>
2021-10-22 09:31:11 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Ensemble Model for Emergency department Trauma Triage (EMETT)',
+    author='Ludvig Wärnberg Gerdin',
+    license='MIT',
+)

023536bbcaa202b774897c0fe7e8a5ed8e729e81
Ludvig Wärnberg Gerdin <ludviggerdin@gmail.com>
2022-01-05 11:33:05 +0100

    Add logging, change name of ensemble class

--- src/models/classifiers.py
+++ src/models/classifiers.py
@@ -2,10 +2,11 @@ import numpy as np
 import pandas as pd
 from tqdm.notebook import tqdm
 
+from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import cross_val_predict, StratifiedKFold
 
 
-class StackingGeneralization():
+class StackedGeneralizationClassifier():
     """Stacking Generalization Classifier.
     
     Args:
@@ -33,9 +34,13 @@ class StackingGeneralization():
         
         # Updated with each run. Lastly set to best hyper parameters if refit.
         self.hyper_parameters = None
+        
+        # Helpers for logging
+        self.__i = 0
+        self.__j = 0
+        self.__n = None
 
-
-    def fit(self, X: pd.DataFrame, y: pd.Series):
+    def fit(self, X: pd.DataFrame, y: pd.Series) -> object:
         """Fits the classifiers and the meta classifier.
         
         Args:
@@ -43,7 +48,7 @@ class StackingGeneralization():
             y: Target labels.
         
         Returns:
-            Returns the classifier itself.
+            The StackedGeneralizationClassifier itself.
         """
         # Set the hyper hyper parameters of the base classifiers
         for clfk in self.base_clfs.keys():
@@ -55,6 +60,9 @@ class StackingGeneralization():
     
         # Get meta features from features
         X_meta = self.cv_inner_loop(X = X, y = y)
+        
+        # Fit each base classifier to all features
+        self.base_clfs__ = [clf.fit(X, y) for clf in self.base_clfs__]
 
         # Fit meta classifier to meta features of train
         self.meta_clf__.fit(X_meta, y)
@@ -62,7 +70,7 @@ class StackingGeneralization():
         return self
 
 
-    def predict_meta_features(self, X):
+    def predict_meta_features(self, X) -> np.ndarray:
         """Uses base classifiers to get meta features.
         
         Args:
@@ -71,14 +79,15 @@ class StackingGeneralization():
         Returns:
             Meta features, i.e. the predicted probabilities by base
             classifiers.
-        
         """
         per_model_predictions = []
+
         for clf in self.base_clfs__:
             prediction = clf.predict_proba(X)[:, 1]
             per_model_predictions.append(prediction[:, np.newaxis])
         
         predictions = np.hstack(per_model_predictions)
+
         return predictions
         
 
@@ -92,8 +101,8 @@ class StackingGeneralization():
             Tuple of predicted proability of 1s and binned predictions.
         """
         # Predict using validation meta features
-        y_pred_con = self.meta_clf__.predict_proba(X)
-        # Calculate AUC of ROC for cut predictions
+        X_meta = self.predict_meta_features(X)
+        y_pred_con = self.meta_clf__.predict_proba(X_meta)
         y_pred_cut = pd.cut(
             x=y_pred_con[:, 1],
             bins=self.hyper_parameters["breaks"],
@@ -111,9 +120,21 @@ class StackingGeneralization():
             X: Features.
             y: Target labels.
         Returns:
-          Each column represent predictions by each respective classifier
+          Array where each column correspond to the predictions by each
+              respective classifier
         """
+        if self.__i == 0 and self.__j == 0:
+            print("Inner loop:")
+            for k in self.__n.index:
+                p = round(self.__n[k] / sum(self.__n) * 100, 2)
+                print("\tNumber of {c}'s: ~{v} ({p}%)".format(
+                    c=round(k),
+                    v=round(self.__n[k] / self.inner_folds),
+                    p=p
+                ))
+
         X_meta = np.zeros((len(X.index), ))
+
         for clf in self.base_clfs__:
             if self.verbose: print("Running predictions for " + str(clf))
             predictions = cross_val_predict(
@@ -131,8 +152,23 @@ class StackingGeneralization():
 
 
     def cv_outer_loop(self, all_hyper_parameters: list,
-                      X: pd.DataFrame, y: pd.Series, refit=True):
-        """Runs outer cross-validation to get best break points."""
+                      X: pd.DataFrame, y: pd.Series, refit=True) -> object:
+        """Runs outer cross-validation.
+        
+        Gets the break points for continous proabilities that 
+        yields the greatest AUC of ROC.
+        
+        Args:
+            all_hyper_parameters: Hyper parameters to try for the base
+                classifiers
+            X: Features.
+            y: Targets.
+            refit: If True, the base classifiers and the meta classifier
+                is as a final step refit to the training set.¨
+                
+        Returns:
+            The StackedGeneralizationClassifier itself.
+        """
         ## Setup for recording auc from each combination of hps
         roc_aucs = pd.DataFrame(
             data = np.zeros((len(all_hyper_parameters), self.outer_folds)),
@@ -141,26 +177,38 @@ class StackingGeneralization():
         
         for i, hyper_parameters in enumerate(tqdm(all_hyper_parameters)):
 
+            self.__i = i 
+
             self.hyper_parameters = hyper_parameters
 
             for j, (train_index, val_index) in enumerate(self.outer_loop.split(X, y)):
+
+                self.__j = j
             
                 X_train = X.iloc[train_index]
                 y_train = y.iloc[train_index]
                 X_val = X.iloc[val_index]
                 y_val = y.iloc[val_index]
                 
+                # Number of each class w. percentage. Only first loop
+                if i == 0 and j == 0:
+                    n = y_val.value_counts()
+                    self.__n = n
+                    print("Outer loop:")
+                    for k in n.index:
+                        p = round(n[k] / sum(n) * 100, 2)
+                        print("\tNumber of {c}'s: ~{v} ({p}%)".format(
+                            c=round(k),v=n[k],p=p)
+                        )
+                
                 self.fit(X = X_train, y = y_train)
-                
-                X_val_meta = self.predict_meta_features(X = X_val)
-                
-                y_pred_con, y_pred_cut = self.predict(X=X_val_meta)
+                y_pred_con, y_pred_cut = self.predict(X=X_val)
     
                 auc = roc_auc_score(
                     y_true=y_val,
                     y_score=y_pred_cut
                 )
-                roc_aucs.iloc[i, j] = 1 - auc
+                roc_aucs.iloc[i, j] = auc
         
         self.roc_aucs = roc_aucs
     

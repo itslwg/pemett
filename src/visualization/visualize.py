73d7ca79c8ef0f28de482c91048706028323ac8c
Ludvig Wärnberg Gerdin <ludviggerdin@gmail.com>
2022-01-06 11:14:43 +0100

    Add vizualization of roc curves for each base clf

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -25,6 +25,28 @@ def plot_roc(y_true, y_pred):
     return p
 
 
+def plot_roc_base_clfs(y_preds, y_true, keys):
+    """Plot ROC for each base classifier."""
+    def f(p, y_true):
+        fpr, tpr, _ = roc_curve(y_true = y_true, y_score = p)
+        a = np.column_stack((fpr, tpr))
+        df = pd.DataFrame(a, columns=["fpr", "tpr"])
+        return df
+
+    df = pd.DataFrame(y_preds)
+    dfs = [f(df[l], y_true = y_true) for l in df.columns]
+    df_plt = pd.concat(dfs, keys=keys).reset_index(level=0)
+    plt = (
+        ggplot(df_plt, aes(x = "fpr",y = "tpr", color = "level_0")) +
+        geom_line() + 
+        xlab("False Positive Rate") + 
+        ylab("True Positive Rate") +
+        theme(legend_title=element_blank())
+    )
+
+    return plt
+
+
 def plot_precision_recall(y_true, y_pred):
     """Plots precision and recall using plotnine."""
     prec, rec, thres = precision_recall_curve(y_true = y_true, probas_pred = 1 - y_pred)
@@ -39,5 +61,6 @@ def plot_precision_recall(y_true, y_pred):
         ylab("Recall")
     )
     return p
+
 
     
